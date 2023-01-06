1bc6640544dee158b438fd5e077fde5b35dadeb3
Tyler Lee <tylerdumbutt@gmail.com>
2023-01-05 19:12:56 -0600

    read chapter 1 and created a hellow world script

---
+++ Chapter 1/hello_cargo/target/debug/deps/hello_cargo-cbb7ad010d13989c.d
@@ -0,0 +1,5 @@
+/home/tlee/Documents/rust-learning/Chapter 1/hello_cargo/target/debug/deps/hello_cargo-cbb7ad010d13989c: src/main.rs
+
+/home/tlee/Documents/rust-learning/Chapter 1/hello_cargo/target/debug/deps/hello_cargo-cbb7ad010d13989c.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter 1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/tlee/Documents/rust-learning/Chapter\ 1/hello_cargo/target/debug/hello_cargo: /home/tlee/Documents/rust-learning/Chapter\ 1/hello_cargo/src/main.rs
